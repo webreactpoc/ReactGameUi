1c0086e73172de9d03c3391b9df8b0d3be54aa9e
Marc-Antoine Dufresne Gagnon <mdufresnegagnon@gmail.com>
2019-06-28 16:22:53 -0400

    added clickable start button

--- src/js/ReactTimeTest.js
+++ src/js/ReactTimeTest.js
@@ -1,9 +1,22 @@
-function printMousePos(event) {
-    document.body.textContent =
-        "( X: " + event.clientX +
-        ", Y: " + event.clientY + " )";
-  }
+// function printMousePos(event) {
+//     document.body.textContent =
+//         "( X: " + event.clientX +
+//         ", Y: " + event.clientY + " )";
+//   }
+
+function loadBeginState(){
+    // Initialyse and create startGameButton and 
+    var startGameButton = document.createElement("startGameButton");
+    startGameButton.innerHTML = "Begin";
+    document.getElementsByTagName("body")[0].appendChild(startGameButton);
+    startGameButton.addEventListener("click", startGameButtonFunction);
+}
+
+
+function startGameButtonFunction(){
+    alert("Clicked");
+}
 
 window.onload = function(){
-    document.addEventListener("click", printMousePos);
+    loadBeginState();
 }
